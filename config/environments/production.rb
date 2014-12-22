f78df77b4fed115ba8141504a5e0914614d8a23c
WeiHsi Hu <weihsi.hu@gmail.com>
2014-12-22 22:50:17 +0000

    Fix productionRB

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Nomster::Application.configure do
-  config.action_mailer.defualt_url_options = { :host => 'nomster-waywho.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'nomster-waywho.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
