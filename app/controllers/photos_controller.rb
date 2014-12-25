89c89f6a8fb3cbbf158ad8e5c6ffdb833e2cb888
WeiHsi <weihsi.hu@gmail.com>
2014-12-25 23:30:30 +0000

    Authentication for photo upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,12 @@ class PhotosController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@place = Place.find(params[:place_id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
+
 		@place.photos.create(photo_params)
+		
 		redirect_to place_path(@place)
 	end         
 
